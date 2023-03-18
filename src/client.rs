2e847de6b293ef28c05a5fd6fb462c29c034666e
Scott Williams <scott@mightydevco.com>
2023-03-18 08:05:40 -0500

    now creating {HOME}/.broadside/accounts directory

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::Cursor;
 use std::path::{Path, PathBuf};
 use walkdir::WalkDir;
@@ -13,8 +14,29 @@ impl Client {
         Client{}
     }
 
+    pub fn repo(&self) -> Result<PathBuf,anyhow::Error> {
+
+        let mut path = PathBuf::new();
+        path.push( format!("{}/.broadside",dirs::home_dir().ok_or(anyhow!("cannot determine home_dir"))?.to_str().ok_or(anyhow!("cannot convert homedir to_str"))?));
+        Ok(path)
+    }
+
+    pub fn account_dir(&self) -> Result<PathBuf,anyhow::Error> {
+        let mut path = self.repo()?;
+
+        path.push("accounts");
+
+        Ok(path)
+    }
+
+
     pub fn publish( &self, path: PathBuf) -> Result<(), anyhow::Error>
     {
+
+        fs::create_dir_all(self.account_dir()?)?;
+
+
+        println!( "$HOME {}", dirs::home_dir().unwrap().to_str().unwrap());
 
         println!("Publishing Dir: {}", path.to_str().unwrap());
 
